adbb112342b35a2e64992db4627b661dae7b36f3
K.Kit <74540801+KohakuKitsune@users.noreply.github.com>
2022-01-09 18:19:53 +0800

    Update 字根與子字串.c

--- 字根與子字串.c
+++ 字根與子字串.c
@@ -1,26 +1,63 @@
 #include<stdio.h>
-#include<stdlib.h>
 
-void Strcmp(char* Pat,char *Str);
+#define YES 1
+#define NO 0
+
+int Strlen(char *str);          //calculate the length of string
+void Strstr(char *s, char *t);  //determine if s is a substring of t
 
 int main()
 {
-  char Pat[100];
-  char Str[100];
+  char s[100];
+  char t[100];
   
-  scanf("%s",Pat);
-  scanf("%s",Str);
+  scanf("%s",s);  //read pattern string
+  scanf("%s",t);  //read test string
   
-  Strcmp(Pat,Str);
+  Strstr(s,t);
+  
   return 0;
 }
- 
-void Strcmp(char* Pat,char *Str)
-{
- char* ptr = Pat;
- char* qtr = Str;
- 
- 
-  
 
+int Strlen(char *str)
+{
+  int Len = 0;
+  char *ptr = str; 
+  while(*ptr)    //repeat until the end of content string
+  {
+    Len +=1;
+    ptr++;
+    if(*ptr == '\n') Len -=1;  //prevent "\n" being counted as a character
+  }
+  return Len;
+}
+
+void Strstr(char *s, char *t)
+{
+  char *ptr = t;
+  int S_Len = Strlen(s);  //calculate the length of pattern string
+  int result = NO;
+  
+  while(*ptr)    //repeat until the end of content t
+  {
+    if(*ptr == *s)
+    {
+      for(int i = 0; i < S_Len; i++)  //repeat until all characters of pattern string are being compared
+      {
+        if(*ptr != *(s+i)) break;     //if there is a mismatch then terminate the for loop
+        else 
+        {
+            if(i == S_Len -1) result = YES;  //if there is a complete pattern of s in t 
+            ptr++;
+        }
+      }
+    }
+    if(result == YES)    //if s proven to be  a substring of t
+    {
+      printf("YES\n");
+      return;
+    }
+    ptr++;
+  }
+  printf("NO\n");
 }
