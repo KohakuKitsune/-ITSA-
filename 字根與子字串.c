aed2cfb5fb6f6e8b54ebd81f5a5387c697cf9435
K.Kit <74540801+KohakuKitsune@users.noreply.github.com>
2022-01-09 17:31:03 +0800

    Add files via upload

---
+++ 字根與子字串.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void Strcmp(char* Pat,char *Str);
+
+int main()
+{
+  char Pat[100];
+  char Str[100];
+  
+  scanf("%s",Pat);
+  scanf("%s",Str);
+  
+  Strcmp(Pat,Str);
+  return 0;
+}
+ 
+void Strcmp(char* Pat,char *Str)
+{
+ char* ptr = Pat;
+ char* qtr = Str;
+ 
+ 
+  
+
+}
